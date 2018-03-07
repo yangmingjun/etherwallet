85ec5aee168e3aa6cc474c16053039d8d18b78f1
yangmingjun <ssyymj@gmail.com>
2018-03-07 14:37:26 +0800

    init

---
+++ config.js
@@ -0,0 +1,8 @@
+var config = {};
+
+//etherscan config
+config.etherscan = {};
+//apikey，https://etherscan.io/myapikey
+config.etherscan.apikey = 'K8AXY1WB3UJRNMFRWKX71RKHBPHC2EK7X4';
+
+module.exports = config;
